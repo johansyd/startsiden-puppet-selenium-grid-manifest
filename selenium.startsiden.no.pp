ba0c15db4fc60b8294ba370af918135ec61ad804
Johan Sydseter <johan.sydseter@startsiden.no>
2013-02-27 23:47:53 +0100

    initial commit

---
+++ selenium.startsiden.no.pp
@@ -0,0 +1,10 @@
+# == node: vagrant-selenium-debian-wheezy-amd64
+# vagrant environment for puppet development
+# === Authors
+# Johan Sydseter <johan.sydseter@startsiden.no>
+node 'selenium' {
+#  Class['first'] ->
+#  Class['test']
+  #Class['java'] ->
+
+}
